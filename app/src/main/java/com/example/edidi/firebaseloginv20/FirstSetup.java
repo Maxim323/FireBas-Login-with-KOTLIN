0c2da4938cf4d321911dd2286d57422a6b22cfbd
Maxim32 <44479059+Maxim323@users.noreply.github.com>
2018-11-14 21:33:39 +0200

    ImageUploud - 40% Done
    Imaginea apare in .xml, dar cand dau build, in mod ciudat nu mai apare pe emulator.

--- app/src/main/java/com/example/edidi/firebaseloginv20/FirstSetup.java
+++ app/src/main/java/com/example/edidi/firebaseloginv20/FirstSetup.java
@@ -1,81 +1,105 @@
 package com.example.edidi.firebaseloginv20;
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.Toolbar;
+
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
 import java.util.HashMap;
 import java.util.Map;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class FirstSetup extends AppCompatActivity {
 
-    private static final String KEY_NUME_PRENUME = "Nume si prenume";
-    private static final String KEY_FACULTATE = "Facultate";
-    private static final String KEY_SPECIALIZARE = "Specializare";
-    private static final String KEY_AN = "An";
-    private static final String KEY_GRUPA = "Grupa";
 
-    private EditText editTextaAn;
-    private EditText editTextNume_Prenume;
-    private EditText editTextFacultate;
-    private EditText editTextSpecializare;
-    private EditText editTextGrupa;
-
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
-
+    private CircleImageView setupImage;
+    private Uri mainImageURI= null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_setup);
 
+        Toolbar setupToolbar = findViewById(R.id.setupToolbar);
 
-        editTextNume_Prenume = findViewById(R.id.edit_text_nume_prenume);
-        editTextGrupa = findViewById(R.id.edit_text_grupa);
-        editTextaAn = findViewById(R.id.edit_text_an);
-        editTextFacultate = findViewById(R.id.edit_text_facultate);
-        editTextSpecializare = findViewById(R.id.edit_text_specializare);
+        setupImage = findViewById(R.id.setup_image);
+
+        setupImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+
+                    if(ContextCompat.checkSelfPermission(FirstSetup.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+
+                        Toast.makeText(FirstSetup.this, "Permission Denied", Toast.LENGTH_LONG).show();
+                        ActivityCompat.requestPermissions(FirstSetup.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+
+                    } else {
+
+                        CropImage.activity()
+                                .setGuidelines(CropImageView.Guidelines.ON)
+                                .setAspectRatio(1,1)
+                                .start(FirstSetup.this);
+
+                    }
+
+                } else {
+
+
+                }
+
+            }
+
+        });
+
 
     }//main function
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+
+                mainImageURI= result.getUri();
+                setupImage.setImageURI(mainImageURI);
 
 
-    public void saveNote(View view) {
-        String name_presume = editTextNume_Prenume.getText().toString();
-        String facultate = editTextFacultate.getText().toString();
-        String specializare = editTextSpecializare.getText().toString();
-        String an = editTextaAn.getText().toString();
-        String grupa = editTextGrupa.getText().toString();
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+
+                Exception error = result.getError();
+
+            }
+        }
 
 
-        Map<String, Object> note = new HashMap<>();
-        note.put(KEY_FACULTATE, facultate);
-        note.put(KEY_SPECIALIZARE, specializare);
-        note.put(KEY_GRUPA, grupa);
-        note.put(KEY_AN, an);
-        note.put(KEY_NUME_PRENUME, name_presume);
 
-
-        db.collection("Utilizatori").document(String.valueOf(name_presume)).set(note)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Toast.makeText(FirstSetup.this, "Note saved", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(FirstSetup.this, "Error!", Toast.LENGTH_SHORT).show();
-
-                    }
-                });
     }
+
+
 }
 
 
