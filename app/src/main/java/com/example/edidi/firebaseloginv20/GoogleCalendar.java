98821e164c536189284fe8f6570cf6f96429e5e8
Maxim32 <44479059+Maxim323@users.noreply.github.com>
2018-11-14 18:28:19 +0200

    Functia de calendar, copy button`ul
    Din nu stiu ce motiv imi da crash cand rulez, dar in rest nu am erori....ce naiba.

--- app/src/main/java/com/example/edidi/firebaseloginv20/GoogleCalendar.java
+++ app/src/main/java/com/example/edidi/firebaseloginv20/GoogleCalendar.java
@@ -3,19 +3,19 @@ package com.example.edidi.firebaseloginv20;
 import android.annotation.SuppressLint;
 import android.content.ClipData;
 import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class GoogleCalendar extends AppCompatActivity {
 
     private EditText editText;
-    private Button button_copy;
-    private ClipboardManager cbm;
-    private ClipData cd;
+    private Button button_copy1;
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -24,18 +24,16 @@ public class GoogleCalendar extends AppCompatActivity {
         setContentView(R.layout.activity_google_calendar);
 
         editText = findViewById(R.id.text_linkcalendar);
-        button_copy = findViewById(R.id.button_copy);
+        button_copy1 = findViewById(R.id.button_copy);
 
-        cbm = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
-
-
-
-        button_copy.setOnClickListener(new View.OnClickListener() {
+        //nu stiu de ce iti da crash la aplciatie...totul pare corect
+        button_copy1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                String text = editText.getText().toString();
-                cd = ClipData.newPlainText("text",text);
-                cbm.setPrimaryClip(cd);
+            public void onClick(View view) {
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("text", editText.getText().toString());
+                clipboard.setPrimaryClip(clip);
+                Toast.makeText(GoogleCalendar.this, "TextCopied", Toast.LENGTH_SHORT).show();
             }
 
         });
