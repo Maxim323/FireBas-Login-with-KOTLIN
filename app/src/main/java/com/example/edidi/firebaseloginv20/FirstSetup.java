5821d3d86d674add5b42aa5ccaa8c8beefd5f710
Maxim32 <44479059+Maxim323@users.noreply.github.com>
2018-11-15 23:07:28 +0200

    Damnn, so close with this picture uploud.
    Cauta linia de comentariu din FirstSetup.

--- app/src/main/java/com/example/edidi/firebaseloginv20/FirstSetup.java
+++ app/src/main/java/com/example/edidi/firebaseloginv20/FirstSetup.java
@@ -1,31 +1,36 @@
 package com.example.edidi.firebaseloginv20;
 import android.Manifest;
-import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
-import android.widget.Toolbar;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.net.URI;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+
 
 
 public class FirstSetup extends AppCompatActivity {
@@ -33,15 +38,60 @@ public class FirstSetup extends AppCompatActivity {
 
     private CircleImageView setupImage;
     private Uri mainImageURI= null;
+    private EditText setupName;
+    private Button setupButton;
+    private StorageReference storageReference;
+    private FirebaseAuth firebaseAuth;
+    private ProgressBar setupProgress;
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_setup);
 
-        Toolbar setupToolbar = findViewById(R.id.setupToolbar);
+        firebaseAuth = FirebaseAuth.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference();
 
         setupImage = findViewById(R.id.setup_image);
+        setupName = findViewById(R.id.setup_name);
+        setupButton = findViewById(R.id.setup_button);
+        setupProgress = findViewById(R.id.setup_progress);
+
+        setupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user_name = setupName.getText().toString();
+
+                if (!TextUtils.isEmpty(user_name) && mainImageURI != null) {
+
+                        setupProgress.setVisibility(View.VISIBLE);
+                        String user_id = firebaseAuth.getCurrentUser().getUid();
+                        StorageReference image_path = storageReference.child("profile_pictures").child(user_id + ".jpg");
+                        image_path.putFile(mainImageURI).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                                if(task.isSuccessful()){
+                                    //AICI DA CRASHH
+                                    Uri download_uri = task.getResult().getUploadSessionUri();
+                                    Toast.makeText(FirstSetup.this,"The imageisuplouded:", Toast.LENGTH_LONG).show();
+                                }else {
+
+                                    String error = task.getException().getMessage();
+                                    Toast.makeText(FirstSetup.this,"Error:" + error, Toast.LENGTH_LONG).show();
+                                }
+
+                                setupProgress.setVisibility(View.INVISIBLE);
+                            }
+                        });
+
+
+                }
+
+            }
+        });
+
 
         setupImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,6 +121,9 @@ public class FirstSetup extends AppCompatActivity {
             }
 
         });
+
+
+
 
 
     }//main function
